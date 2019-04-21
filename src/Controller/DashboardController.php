21a4457cb63d8826d109fec86a69dba30a663999
David Castro <davidcv191@gmail.com>
2019-04-21 20:28:50 +0200

    Use date range picker lib on dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,28 +7,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Translation\TranslatorInterface;
+use DateTime;
 
 class DashboardController extends AbstractController
 {
     /**
      * @Route("/dashboard", name="view_dashboard")
      */
-    public function index(SessionInterface $session, ConsumptionStatsHelper $consumptionStatsHelper)
+    public function index(SessionInterface $session, ConsumptionStatsHelper $consumptionStatsHelper, TranslatorInterface $translator)
     {
         if (!$this->isGranted('ROLE_USER')) {
             return $this->redirectToRoute('app_login');
         }
 
-        $datetimeFrom = (new \DateTime('midnight'))
+        $datetimeFrom = (new DateTime('midnight'))
             ->modify("-10 days");
-        $datetimeTo = new \DateTime();
+        $datetimeTo = new DateTime();
 
         $repository = $this->getDoctrine()->getRepository(Consumption::class);
         $consumptionCollection = $repository->findByDatetimeInterval($datetimeFrom, $datetimeTo);
 
         return $this->render('dashboard/dashboard.html.twig', [
             'consumption_last_days' => $consumptionStatsHelper->getAggregatedConsumptionByDate($consumptionCollection),
-            'current_date' => (new \DateTime())->format('Y-m-d')
+            'current_date' => (new DateTime())->format($translator->trans('app.date_format'))
         ]);
     }
 
